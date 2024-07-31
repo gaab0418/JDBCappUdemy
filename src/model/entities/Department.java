91a2e21766a2b92f339b7bc322f61258d8683c71
gaab0418 <zgabrielw.sz13@gmail.com>
2024-07-31 20:17:25 -0300

    .

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,2 +1,55 @@
-package model.entities;public class Department {
+package model.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+    private int id;
+    private String name;
+
+    public Department() {
+
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department department = (Department) o;
+        return id == department.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,2 +1,103 @@
-package model.entities;public class Seller {
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String email;
+    private Date bithDay;
+    private Double baseSalary;
+    private Department department;
+
+    public Seller() {}
+
+    public Seller(int id, String name, String email, Date bithDay, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bithDay = bithDay;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", bithDay=" + bithDay +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return id == seller.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBithDay() {
+        return bithDay;
+    }
+
+    public void setBithDay(Date bithDay) {
+        this.bithDay = bithDay;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
